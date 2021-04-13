6e74ebb8e78dc6f8edd499e1af1baabd5a3cc9cc
Alexey Sergeev <aleksei_s@bk.ru>
2021-04-13 15:03:33 +0300

    Initial Commit

---
+++ MealTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealTracker
+//
+//  Created by Алексей Сергеев on 13.04.2021.
+//  Copyright © 2021 Алексей Сергеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
